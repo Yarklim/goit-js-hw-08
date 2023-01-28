427c2d643a9dc0905f003a4883632f82ba03eae1
Yaroslav Klimas <yarklim@gmail.com>
2023-01-28 17:16:24 +0200

    added task 1 solution

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector('.gallery');
+
+// Создаю разметку галлереи в HTML
+const imgMarcup = createItemImg(galleryItems);
+galleryListEl.insertAdjacentHTML('beforeend', imgMarcup);
+
+// Функция для создания разметки изображения
+function createItemImg(items) {
+  const imgItem = items
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src='${preview}'
+      alt='${description}'
+    />
+  </a>`;
+    })
+    .join('');
+
+  return imgItem;
+}
+
+// Библиотека SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
