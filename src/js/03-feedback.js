174f3ac18421158431a26dc737cc21e0c979ba99
Yaroslav Klimas <yarklim@gmail.com>
2023-01-30 19:00:17 +0200

    added task 3 solution

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,31 +4,44 @@ const formEl = document.querySelector('.feedback-form'); // Ссылка на э
 formEl.addEventListener('input', throttle(onFormData, 500)); // Отслеживаю событие инпут
 formEl.addEventListener('submit', onSubmitForm); // Отслеживаю событие сабмит
 
-const formData = {}; // Объект для хранения значений полей формы
+// Ссылки на инпут и текстовое поле
+const inputEmail = document.querySelector('.feedback-form input');
+const textareaMessage = document.querySelector('.feedback-form textarea');
+
+// Ключ для хранилища
+const STORAGE_KEY = 'feedback-from-state';
+
+const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {}; // Объект для хранения значений полей формы
 
 // Сохраняю в объект текущее значение поля формы и записываю объект в локальное хранилище
 function onFormData(e) {
   formData[e.target.name] = e.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 // Очищаю хранилище и поля формы при сабмите, вывожу в консоль объект formData
 function onSubmitForm(e) {
-  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
   e.preventDefault();
   e.currentTarget.reset();
-  localStorage.removeItem('feedback-form-state');
+  localStorage.removeItem(STORAGE_KEY);
 }
 
 // Проверяю состояние хранилища и заполняю поля формы
-(function dataFromLocalStorage() {
-  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
-  const email = document.querySelector('.feedback-form input');
-  const message = document.querySelector('.feedback-form textarea');
-  if (data?.email) {
-    email.value = data.email;
+function dataFromLocalStorage() {
+  try {
+    const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+    if (data?.email) {
+      inputEmail.value = data.email;
+    }
+    if (data?.message) {
+      textareaMessage.value = data.message;
+    }
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
   }
-  if (data?.message) {
-    message.value = data.message;
-  }
-})();
+}
+
+dataFromLocalStorage();
